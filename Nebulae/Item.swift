a25c1d92be26559a37ecdee74c3b039f6ba31452
Denisa Cotiga <denisacotiga@yahoo.com>
2026-06-23 13:48:59 -0500

    Initial Commit

---
+++ Nebulae/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Nebulae
+//
+//  Created by Denisa Cotiga on 6/23/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ NebulaeTests/NebulaeTests.swift
@@ -0,0 +1,18 @@
+//
+//  NebulaeTests.swift
+//  NebulaeTests
+//
+//  Created by Denisa Cotiga on 6/23/26.
+//
+
+import Testing
+
+struct NebulaeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
